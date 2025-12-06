a26314f574a5cdc53f6bb9fa76c6e7e6b120ecb2
DocMX <jorge_cruzer@hotmail.com>
2025-12-05 22:41:41 -0600

    rspec

--- spec/easybroker_client_spec.rb
+++ spec/easybroker_client_spec.rb
@@ -1,23 +1,38 @@
 require 'rspec'
 require 'webmock/rspec'
-require_relative '../lib/easybroker_client'
+require_relative '../lib/easy_broker/client'
 
-RSpec.describe EasyBrokerClient do
+RSpec.describe EasyBroker::Client do
   let(:api_key) { 'test_key' }
   let(:base) { 'https://api.stagingeb.com' }
+
   subject { described_class.new(api_key: api_key, base_url: base, limit: 2) }
 
   before do
     stub_request(:get, "#{base}/v1/properties")
-      .with(query: hash_including({'page' => '1', 'limit' => '2'}), headers: {'X-Authorization' => api_key})
-      .to_return(status: 200, body: [
-        { 'public_id' => 'EB-1', 'title' => 'Casa en DGO 1' },
-        { 'public_id' => 'EB-2', 'title' => 'Departamento en DGO 2' }
-      ].to_json, headers: {'Content-Type' => 'application/json'})
+      .with(
+        query: hash_including({ 'page' => '1', 'limit' => '2' }),
+        headers: { 'X-Authorization' => api_key }
+      )
+      .to_return(
+        status: 200,
+        body: [
+          { 'public_id' => 'EB-1', 'title' => 'Casa en DGO 1' },
+          { 'public_id' => 'EB-2', 'title' => 'Departamento en DGO 2' }
+        ].to_json,
+        headers: { 'Content-Type' => 'application/json' }
+      )
 
     stub_request(:get, "#{base}/v1/properties")
-      .with(query: hash_including({'page' => '2', 'limit' => '2'}), headers: {'X-Authorization' => api_key})
-      .to_return(status: 200, body: [].to_json, headers: {'Content-Type' => 'application/json'})
+      .with(
+        query: hash_including({ 'page' => '2', 'limit' => '2' }),
+        headers: { 'X-Authorization' => api_key }
+      )
+      .to_return(
+        status: 200,
+        body: [].to_json,
+        headers: { 'Content-Type' => 'application/json' }
+      )
   end
 
   it 'iterar todas las propiedades y devuelve títulos' do
